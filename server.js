24da9f7a7b294ab8eb8edaa8c7134e97958af617
Daryll  Osis <dosis@csumb.edu>
2019-09-13 23:53:09 -0700

    version 1.0.0 trying to deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,23 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-const PORT = process.env.PORT || 5000;
-
 app.get('/', (req, res) => res.send('App Running'));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/shoes', require('./routes/api/shoes'));
+
+// Serve static assets if in production.      ----------> this is for deployment
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
