a4ada26387b4adeeba19779487aa7974d50586eb
Daryll  Osis <dosis@csumb.edu>
2019-09-20 01:37:11 -0700

    trying to deploy this app

--- server.js
+++ server.js
@@ -22,11 +22,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
 const port = process.env.PORT || 5000;
-const host = '0.0.0.0';
 
-app.listen(port, host, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
