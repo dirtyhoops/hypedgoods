8e54952c649d429438266bbdff36134af6eaaa6f
Daryll  Osis <dosis@csumb.edu>
2019-09-20 20:03:05 -0700

    trying to deploy this web app

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/shoes', require('./routes/api/shoes'));
 app.use('/api/orders', require('./routes/api/orders'));
 
-// app.use(express.static(path.join(__dirname, 'client/build')));
 // Serve static assets if in production.      ----------> this is for deployment
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
